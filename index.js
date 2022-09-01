fd61842b2344a00cebceeb1137cb8b79d37af4de
Le Viet Hoang <hashdotlee@gmail.com>
2022-09-01 12:53:49 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageCreate", async (message) => {
+  console.log("message created");
+  console.log(message);
+  if (message.content === "ping") {
+    await message.reply("Pong!");
+  }
+});
+
+
+client.login(process.env.TOKEN);
