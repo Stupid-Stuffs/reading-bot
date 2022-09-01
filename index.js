161f7e2adaa820e89ea39e09095fdd7037d141f1
Le Viet Hoang <hashdotlee@gmail.com>
2022-09-01 16:10:26 +0700

    feat: db and regex

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-require('dotenv').config()
+require("dotenv").config();
+
 const { Client, GatewayIntentBits } = require("discord.js");
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+// connect DB
+const db = require("./DB");
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("messageCreate", async (message) => {
-  if (message.content === "ping") {
+  let r = /^Read\s\"\D+\"\sin\s[0-3][0-9]\sdays/;
+  if (message.content.match(r)) {
     await message.reply("Pong!");
   }
 });
-
 
 client.login(process.env.TOKEN);
