587b922ac9f23f6317d198d641564f6ea95765b8
Le Viet Hoang <hashdotlee@gmail.com>
2022-09-01 23:24:48 +0700

    update commands

--- DB.js
+++ DB.js
@@ -1,7 +1,19 @@
-const { Pool } = require('pg')
-const pool = new Pool()
+require("dotenv").config();
+// const fs = require('node:fs');
+const { Pool } = require("pg");
+/*const config = {
+  database: process.env.PGDATABASE,
+  host: process.env.PGHOST,
+  // this object will be passed to the TLSSocket constructor
+  ssl: {
+    rejectUnauthorized: false,
+    key: fs.readFileSync("../server.key").toString(),
+    cert: fs.readFileSync("../server.crt").toString(),
+  },
+};*/
+const pool = new Pool(/*config*/);
 module.exports = {
   query: (text, params, callback) => {
-    return pool.query(text, params, callback)
+    return pool.query(text, params, callback);
   },
-}
+};
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 var CronJob = require("cron").CronJob;
 
-const fs = require('node:fs');
-const path = require('node:path');
+const fs = require("node:fs");
+const path = require("node:path");
 const { Client, GatewayIntentBits, Collection } = require("discord.js");
 const client = new Client({
   intents: [
@@ -13,29 +13,31 @@ const client = new Client({
 });
 
 client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
 }
 
 // connect DB
 const db = require("./DB");
 
 var job = new CronJob(
-  "0 22 * * * *",
+  "15 * * * * *",
   async function () {
-    const not_finished_books = await db.query(
+    const { rows } = await db.query(
       `SELECT * 
 		  FROM books
 		  WHERE is_finished = false`
     );
-    console.log(not_finished_books);
+    // console.log(rows);
   },
   null,
   false,
@@ -61,19 +63,22 @@ client.on("messageCreate", async (message) => {
   }
 });
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-	const command = interaction.client.commands.get(interaction.commandName);
+  const command = interaction.client.commands.get(interaction.commandName);
 
-	if (!command) return;
+  if (!command) return;
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
 });
 
 client.login(process.env.TOKEN);
