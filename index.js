5683bf41bf85b70f37b5bf1ca54f2e74af2f3cd4
Le Viet Hoang <hashdotlee@gmail.com>
2022-09-01 13:08:53 +0700

    chore: add package discordjs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const { Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
