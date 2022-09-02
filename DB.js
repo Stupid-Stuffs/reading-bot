60c748361985981024061c30c041ab94efa452fa
Le Viet Hoang <hashdotlee@gmail.com>
2022-09-02 15:33:01 +0700

    fix: remove self-sign cert

--- DB.js
+++ DB.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-const fs = require('node:fs');
 const { Pool } = require("pg");
 const config = {
   database: process.env.PGDATABASE,
@@ -7,8 +6,6 @@ const config = {
   // this object will be passed to the TLSSocket constructor
   ssl: {
     rejectUnauthorized: false,
-    key: fs.readFileSync("../server.key").toString(),
-    cert: fs.readFileSync("../server.crt").toString(),
   },
 };
 const pool = new Pool(config);
